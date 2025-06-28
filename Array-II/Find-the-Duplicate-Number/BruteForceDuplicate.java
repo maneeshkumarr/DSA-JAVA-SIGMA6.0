43bb235540c6403f897db93621fc1a5a211307e3
maneeshkumarr <manishscontacts4@gmail.com>
2025-06-28 14:31:04 +0530

    feat: add brute‑force solution with dry run for Find the Duplicate Number

---
+++ Array-II/Find-the-Duplicate-Number/BruteForceDuplicate.java
@@ -0,0 +1,18 @@
+// BruteForceDuplicate.java
+public class BruteForceDuplicate {
+    public static int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1; // as per constraints, this line shouldn't be reached
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 4, 2, 2};
+        System.out.println("Duplicate Number: " + findDuplicate(nums));
+    }
+}
